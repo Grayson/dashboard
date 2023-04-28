5e8f73a224e7de45e46e1099fbe9680a61e99f05
Grayson Hansard <grayson.hansard@bitrise.io>
2023-04-28 11:29:59 -0400

    Pull PAT from environment

--- generate-pr-alerts/main.go
+++ generate-pr-alerts/main.go
@@ -1,19 +1,62 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
 
 	"github.com/Grayson/dashboard/generate-pr-alerts/lib/github"
+	"gopkg.in/yaml.v2"
 )
 
-func main() {
-	fmt.Println("generate-pr-alerts")
+const (
+	GitHubTokenEnvVar = "GITHUB_TOKEN"
+)
 
-	client := github.NewClient(http.DefaultClient, "redacted")
+type Config struct {
+	Token string `yaml:"token"`
+}
+
+func main() {
+	token, _ := os.LookupEnv(GitHubTokenEnvVar)
+	config, err := getConfig()
+
+	if err != nil {
+		panic(err)
+	}
+
+	if config.Token != "" {
+		token = config.Token
+	}
+
+	actualToken := flag.String("token", token, "GitHub Personal Access Token (can also set via environment variable `GITHUB_TOKEN`)")
+	flag.Parse()
+
+	client := github.NewClient(http.DefaultClient, *actualToken)
 	url, _ := url.Parse("https://api.github.com/repos/Grayson/dashboard/pulls")
 	pulls, err := client.Pulls(url)
-	print(pulls)
-	print(err)
+	fmt.Println(pulls)
+	fmt.Println(err)
+}
+
+func getConfig() (*Config, error) {
+	filename := "config.yaml"
+
+	if _, err := os.Stat(filename); err != nil {
+		return &Config{}, nil
+	}
+
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var c Config
+	if err := yaml.Unmarshal(bytes, &c); err != nil {
+		return nil, err
+	}
+
+	return &c, nil
 }
