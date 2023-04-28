1e7e6a1a761afd3b2e2d9912495341c6146a60f2
Grayson Hansard <grayson.hansard@bitrise.io>
2023-04-28 13:49:47 -0400

    Refactor to make fetching code generic

--- generate-pr-alerts/lib/github/client.go
+++ generate-pr-alerts/lib/github/client.go
@@ -21,35 +21,44 @@ func NewClient(client *http.Client, personalAccessToken string) *Client {
 }
 
 func (c *Client) Pulls(url *url.URL) ([]Pull, error) {
+	w := wrapper[[]Pull]{}
+	return w.get(url, c.personalAccessToken, *c.client)
+}
+
+type wrapper[T any] struct {
+	empty T
+}
+
+func (w *wrapper[T]) get(url *url.URL, personalAccessToken string, client http.Client) (T, error) {
 	req, err := http.NewRequest("GET", url.String(), nil)
 	if err != nil {
-		return []Pull{}, err
+		return w.empty, err
 	}
 
 	req.Header.Add("Accept", "application/vnd.github+json")
-	req.Header.Add("Authorization", fmt.Sprintf("token %s", c.personalAccessToken))
+	req.Header.Add("Authorization", fmt.Sprintf("token %s", personalAccessToken))
 
-	resp, err := c.client.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
-		return []Pull{}, err
+		return w.empty, err
 	}
 
 	defer resp.Body.Close()
 	bytes, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return []Pull{}, err
+		return w.empty, err
 	}
 
 	var errorResponse GithubErrorResponse
 	json.Unmarshal(bytes, &errorResponse)
 	if errorResponse.Message != "" {
-		return []Pull{}, errorResponse
+		return w.empty, errorResponse
 	}
 
-	var pullResponse []Pull
-	if err := json.Unmarshal(bytes, &pullResponse); err != nil {
-		return []Pull{}, err
+	var response T
+	if err := json.Unmarshal(bytes, &response); err != nil {
+		return w.empty, err
 	}
 
-	return pullResponse, nil
+	return response, nil
 }
