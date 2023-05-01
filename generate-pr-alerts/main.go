b79743a1a532f9111eb0d7384fbdece19d14db70
Grayson Hansard <grayson.hansard@bitrise.io>
2023-05-01 09:50:56 -0400

    Demonstrate usage in `main.go`

--- generate-pr-alerts/main.go
+++ generate-pr-alerts/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"net/url"
 	"os"
 
 	"github.com/Grayson/dashboard/generate-pr-alerts/lib/github"
@@ -40,10 +39,23 @@ func main() {
 	}
 
 	client := github.NewClient(http.DefaultClient, *actualToken)
-	url, _ := url.Parse("https://api.github.com/repos/Grayson/dashboard/pulls")
-	pulls, err := client.Pulls(url)
-	fmt.Println(pulls)
-	fmt.Println(err)
+	// url, _ := url.Parse("https://api.github.com/repos/Grayson/dashboard/pulls")
+	// pulls, err := client.Pulls(url)
+	// fmt.Println(pulls)
+	// fmt.Println(err)
+
+	// orgUrl, _ := url.Parse("https://api.github.com/orgs/objectiveceo")
+	// org, orgErr := client.OrganizationInfo(orgUrl)
+	// fmt.Println(org)
+	// fmt.Println(orgErr)
+
+	orgReposUrl, _ := github.OrganizationReposUrl("objectiveceo")
+	orgRepos, orgReposErr := client.OrganizationRepos(orgReposUrl)
+	fmt.Printf("error: %v\n", orgReposErr)
+	for _, x := range orgRepos {
+		fmt.Printf("%#v", x)
+		fmt.Println("-")
+	}
 }
 
 func getConfig() (*Config, error) {
