eed1c4e0325d9ad4b1e3446810225bdc3d3a9c56
Grayson Hansard <grayson.hansard@bitrise.io>
2023-05-02 13:34:56 -0400

    Sort fields

--- generate-pr-alerts/lib/github/organizationrepoinfo.go
+++ generate-pr-alerts/lib/github/organizationrepoinfo.go
@@ -1,12 +1,12 @@
 package github
 
 type OrganizationRepoInfo struct {
-	Id          int    `json:"id,omitempty"`
-	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
 	FullName    string `json:"full_name,omitempty"`
 	GitUrl      string `json:"git_url,omitempty"`
 	HtmlUrl     string `json:"html_url,omitempty"`
-	Description string `json:"description,omitempty"`
-	Url         string `json:"url,omitempty"`
+	Id          int    `json:"id,omitempty"`
+	Name        string `json:"name,omitempty"`
 	PullsUrl    string `json:"pulls_url,omitempty"`
+	Url         string `json:"url,omitempty"`
 }
