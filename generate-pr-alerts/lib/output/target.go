3f259cc2b4e5e2e98d2a050106b740d9895329e4
Grayson <grayson.hansard@gmail.com>
2023-05-02 19:13:08 -0400

    Better fill out start/end phases for pulls and issues

--- generate-pr-alerts/lib/output/target.go
+++ generate-pr-alerts/lib/output/target.go
@@ -16,9 +16,13 @@ type Target interface {
 	VisitOrganization(org *github.OrganizationInfo) error
 	EndOrganizations() error
 
-	StartRepo(repo *github.OrganizationRepoInfo, pulls []github.Pull) error
+	StartRepo(repo *github.OrganizationRepoInfo) error
+	StartPulls(pulls []github.Pull) error
 	VisitPull(pull *github.Pull) error
+	EndPulls() error
+	StartIssues(issues []github.IssuesInfo) error
 	VisitIssue(issue *github.IssuesInfo) error
+	EndIssues() error
 	EndRepo() error
 
 	End() error
