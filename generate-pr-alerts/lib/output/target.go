f227c54a4207157878f8ff8743dc7aec823aa282
Grayson <grayson.hansard@gmail.com>
2023-05-02 19:35:18 -0400

    Add whitespace to aid legibility

--- generate-pr-alerts/lib/output/target.go
+++ generate-pr-alerts/lib/output/target.go
@@ -17,12 +17,15 @@ type Target interface {
 	EndOrganizations() error
 
 	StartRepo(repo *github.OrganizationRepoInfo) error
+
 	StartPulls(pulls []github.Pull) error
 	VisitPull(pull *github.Pull) error
 	EndPulls() error
+
 	StartIssues(issues []github.IssuesInfo) error
 	VisitIssue(issue *github.IssuesInfo) error
 	EndIssues() error
+
 	EndRepo() error
 
 	End() error
