2930504b92f152665f7f1ed9e6aa570a4a2bcbec
Grayson Hansard <grayson.hansard@bitrise.io>
2023-05-05 14:00:39 -0400

    First pass at pulling org repos concurrently

--- generate-pr-alerts/lib/github/client.go
+++ generate-pr-alerts/lib/github/client.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"sync"
+	"sync/atomic"
 )
 
 type Client struct {
@@ -37,29 +39,80 @@ func (c *Client) OrganizationInfo(url *url.URL) (OrganizationInfo, error) {
 	return w.get(url, c.personalAccessToken, *c.client)
 }
 
-func (c *Client) OrganizationRepos(url *url.URL) ([]OrganizationRepoInfo, error) {
+func (c *Client) OrganizationRepos(u *url.URL) ([]OrganizationRepoInfo, error) {
 	const (
 		pageLimit       = 32
 		defaultPageSize = 30
 	)
 
-	info := make([]OrganizationRepoInfo, 0)
+	urls := make(chan *url.URL, 1)
+
+	// Feed urls into the pipeline
+	go func() {
+		for page := 0; page < pageLimit; page++ {
+			url := *u
+			query := url.Query()
+			query.Set("page", strconv.Itoa(page))
+			url.RawQuery = query.Encode()
+			fmt.Printf("Sending %v\n", url.String())
+			urls <- &url
+		}
+		fmt.Println("Closing urls")
+		close(urls)
+	}()
+
+	type x struct {
+		more []OrganizationRepoInfo
+		err  error
+	}
+	ch := make(chan x)
+
 	w := wrapper[[]OrganizationRepoInfo]{}
 
-	for page := 0; page < pageLimit; page++ {
-		query := url.Query()
-		query.Set("page", strconv.Itoa(page))
-		url.RawQuery = query.Encode()
+	wg := sync.WaitGroup{}
+	stop := atomic.Bool{}
+	for worker := 0; worker < 4; worker++ {
+		wg.Add(1)
+		go func(worker int) {
+			defer func() {
+				fmt.Printf("Done %v\n", worker)
+				wg.Done()
+			}()
+			for url := range urls {
+				fmt.Printf("Loading %v on worker %v\n", url, worker)
+				if stop.Load() {
+					fmt.Printf("Stopping worker %v\n", worker)
+					return
+				}
+				m, e := w.get(url, c.personalAccessToken, *c.client)
+				fmt.Println("Sending")
+				ch <- x{m, e}
+			}
+		}(worker)
+	}
+	go func() {
+		fmt.Println("Waiting")
+		wg.Wait()
+		fmt.Println("Closing")
+		close(ch)
+	}()
 
-		more, err := w.get(url, c.personalAccessToken, *c.client)
-		if err != nil {
-			return nil, err
+	info := make([]OrganizationRepoInfo, 0)
+	var err error
+	for x := range ch {
+		if x.err != nil {
+			err = x.err
+		} else {
+			info = append(info, x.more...)
 		}
-		info = append(info, more...)
 
-		if len(more) < defaultPageSize {
-			break
+		if err != nil || (len(x.more) < defaultPageSize) {
+			stop.Store(true)
 		}
+	}
+	fmt.Println("Returning")
+	if err != nil {
+		return nil, err
 	}
 
 	return info, nil
